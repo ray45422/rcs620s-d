ac69f0306b3fdd32dee33a42ac1b4eff6b0fea9f
ray45422 <ray45422@gmail.com>
2018-04-01 21:53:53 +0900

    名前がメチャクチャだったので修正

--- source/nfc/tag/felica/block.d
+++ source/nfc/tag/felica/block.d
@@ -1,7 +1,7 @@
-module nfc.tag.felica.command;
+module nfc.tag.felica.block;
 import std.conv;
 
-struct BlockList{
+struct Block{
 	this(ushort blockNumber, ubyte serviceCodeNumber = 0, ubyte accessMode = 0b00){
 		this.blockNum = blockNumber;
 		this.srvCodeNum = serviceCodeNumber;
@@ -26,11 +26,11 @@ private:
 }
 unittest{
 	//block number
-	assert(BlockList(0).pack == [0b10000000, 0b0]);
-	assert(BlockList(511).pack == [0b0, 0b11111111, 0b1]);
+	assert(Block(0).pack == [0b10000000, 0b0]);
+	assert(Block(511).pack == [0b0, 0b11111111, 0b1]);
 	//service code number
-	assert(BlockList(0, 1).pack == [0b10000001, 0b0]);
-	assert(BlockList(0, 15).pack == [0b10001111, 0b0]);
+	assert(Block(0, 1).pack == [0b10000001, 0b0]);
+	assert(Block(0, 15).pack == [0b10001111, 0b0]);
 	//access mode
-	assert(BlockList(0, 0, 1).pack == [0b10010000, 0b0]);
+	assert(Block(0, 0, 1).pack == [0b10010000, 0b0]);
 }
