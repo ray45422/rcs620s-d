d9eafe6b62f93e7756ae8ffc7e164544f1e48e61
ray45422 <ray45422@gmail.com>
2023-05-01 16:20:06 +0900

    fixtypo
    mobileです

--- source/nfc/tag/felica/felicaStandard.d
+++ source/nfc/tag/felica/felicaStandard.d
@@ -11,9 +11,9 @@ class FeliCaStandard: FeliCa{
 		RC_S960 =ICProduct([0x0d], FeliCaType.FeliCa_STANDARD, "RC-S960"),
 		RC_S962 = ICProduct([0x20], FeliCaType.FeliCa_STANDARD, "RC-S962"),
 		RC_SA00_1 = ICProduct([0x32], FeliCaType.FeliCa_STANDARD,"RC-SA00/1"),
-		MOVILE_V1 = ICProduct([0x06, 0x07], FeliCaType.FeliCa_STANDARD, "Movile FeliCa Version1.0"),
-		MOVILE_V2 = ICProduct([0x10, 0x11, 0x12, 0x13], FeliCaType.FeliCa_STANDARD, "Movile FeliCa Version2.0"),
-		MOVILE_V3 = ICProduct([0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f], FeliCaType.FeliCa_STANDARD, "Movile FeliCa Version3.0"),
+		MOBILE_V1 = ICProduct([0x06, 0x07], FeliCaType.FeliCa_STANDARD, "Mobile FeliCa Version1.0"),
+		MOBILE_V2 = ICProduct([0x10, 0x11, 0x12, 0x13], FeliCaType.FeliCa_STANDARD, "Mobile FeliCa Version2.0"),
+		MOBILE_V3 = ICProduct([0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f], FeliCaType.FeliCa_STANDARD, "Mobile FeliCa Version3.0"),
 		RC_S__ = ICProduct([0x36], FeliCaType.FeliCa_STANDARD, "RC-S???(new ICOCA etc..)"),
 	}
 	this(ubyte[] idm, ubyte[] ppm){
